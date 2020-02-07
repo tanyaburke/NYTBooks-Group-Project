8c6adf77ed568fe69b72948a8da91c90a5e6e331
uwagboe <sm.uwagboe@gmail.com>
2020-02-07 18:25:07 -0500

    addded the view delegate and the data source

--- Controllers/FavoritesViewController.swift
+++ Controllers/FavoritesViewController.swift
@@ -12,20 +12,61 @@ class FavoritesViewController: UIViewController {
     
     // remember to push ONLY through terminal and also
     
-    private let fc = FavoritesView()
-    
     // instance of the controller
-    override func loadView() {
-        view = fc
-    }
+    private let favoriteViewInstance = FavoritesView()
     
+    //making the instance of the empty array for the favs
+    // need model inorder to add what is suppose to be in the array.
+    public var savedFavs = [String]()
+    
+    override func loadView() {
+        view = favoriteViewInstance
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
+        favoriteViewInstance.favsCollectionView.delegate = self
+        favoriteViewInstance.favsCollectionView.dataSource = self
         
+        // need to set the nib for the cell here.
     }
     
+}
 
+extension FavoritesViewController: UICollectionViewDataSource {
+    // amount of items
+    // what is the item
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // savedFavs.count
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "favCell", for: indexPath)
+        let selectedFav = savedFavs[indexPath.row]
 
+        
+        return cell
+    }
+    
+    // will we be doing a configure cell
+    
+}
+
+extension FavoritesViewController: UICollectionViewDelegateFlowLayout {
+    // when I do something then someone else will listen...
+    // delegates listen for what we want ...
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let maxSize: CGSize = UIScreen.main.bounds.size
+        
+        let itemWidth: CGFloat = maxSize.width
+        let itemheight: CGFloat = maxSize.height * 0.30
+        
+        return CGSize(width: itemWidth, height: itemheight)
+    }
+    
+    
 }
