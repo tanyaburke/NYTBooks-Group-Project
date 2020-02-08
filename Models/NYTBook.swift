fe41eff382df7cb1e4e3ae4b01fee2073c60d496
CameronRivera <cameronrivera@pursuit.org>
2020-02-07 21:00:43 -0500

    Added a model, API Client, and asynchronous tests

--- Models/NYTBook.swift
+++ Models/NYTBook.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+
+// Structures involved in getting the category names for the picker view.
+
+struct ListWrapper: Codable{
+    let results: [ListItem]
+}
+
+struct ListItem: Codable, Equatable{
+    let displayName: String
+    let listNameEncoded: String
+    
+    private enum CodingKeys: String, CodingKey{
+        case displayName = "display_name"
+        case listNameEncoded = "list_name_encoded"
+    }
+}
+
+// Structures involved in getting the data for a book.
+
+struct TopLevelBookResult: Codable{
+    let results: BookWrapper
+}
+struct BookWrapper: Codable {
+    let books: [BookData]
+}
+
+struct BookData: Codable, Equatable{
+    let rank: Int
+    let weeksOnList: Int
+    let publisher: String
+    let description: String
+    let title: String
+    let author: String
+    let bookImage: String
+    let bookImageWidth: Int
+    let bookImageHeight: Int
+    let buyLinks: [ProductLink]
+    
+    private enum CodingKeys: String, CodingKey{
+        case rank
+        case weeksOnList = "weeks_on_list"
+        case publisher
+        case description
+        case title
+        case author
+        case bookImage = "book_image"
+        case bookImageWidth = "book_image_width"
+        case bookImageHeight = "book_image_height"
+        case buyLinks = "buy_links"
+    }
+}
+
+struct ProductLink: Codable, Equatable{
+    let name: String
+    let url: String
+}
+
--- NetworkServices/NYAPIClient.swift
+++ NetworkServices/NYAPIClient.swift
@@ -7,3 +7,52 @@
 //
 
 import Foundation
+import NetworkHelper
+
+struct NYTAPIClient{
+    static func getCategories(completion: @escaping (Result<[ListItem],AppError>) -> ()){
+        let endpointURL = "https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=\(APIKeys.NYTimesBestSellersKey)"
+        guard let url = URL(string: endpointURL) else {
+            completion(.failure(.badURL(endpointURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        NetworkHelper.shared.performDataTask(with: request) { result in
+            switch result{
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let listWrapper = try JSONDecoder().decode(ListWrapper.self, from: data)
+                    completion(.success(listWrapper.results))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+    
+    static func getBookData(_ userCategory: String, completion: @escaping (Result<[BookData],AppError>) -> ()){
+        let urlEndpoint = "https://api.nytimes.com/svc/books/v3/lists/current/\(userCategory).json?api-key=\(APIKeys.NYTimesBestSellersKey)"
+        
+        guard let url = URL(string: urlEndpoint) else {
+            completion(.failure(.badURL(urlEndpoint)))
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { result in
+            switch result{
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let bookWrapper = try JSONDecoder().decode(TopLevelBookResult.self, from: data)
+                    completion(.success(bookWrapper.results.books))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
