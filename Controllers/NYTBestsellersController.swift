23c1b101d88a9854458b106ef32565035914994a
Yuliia Engman <yuliia.engman@gmail.com>
2020-02-10 23:40:59 -0500

    updated code with API

--- Controllers/NYTBestsellersController.swift
+++ Controllers/NYTBestsellersController.swift
@@ -12,42 +12,85 @@ class NYTBestsellersController: UIViewController {
     
     private let nytBestSellerView = NYTBestSellersView()
     
-    private let categories = ["Business", "Technology", "Travel", "Authobiography", "Novels"]
+    // FIXME: uncomment when will use data persistance from TabBarController
+    //public var dataPersistence: DataPersistence<BookData>!
+    
+    //FIXME: if we will use userPreference for user defaults
+    //public var userPreference: UserPreference!
+    
+    // is this correct?
+    private var categories = [ListItem]()
+    //{
+    //        didSet {
+    //            DispatchQueue.main.async {
+    //                self.nytBestSellerView.pickerView.reloadAllComponents()
+    //            }
+    //        }
+    //    }
+    
+    // getting data for our collection view from API:
+    private var bookData = [BookData] () {
+        didSet {
+            DispatchQueue.main.async {
+                self.nytBestSellerView.collectionView.reloadData()
+            }
+        }
+    }
     
     override func loadView() {
         view = nytBestSellerView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemOrange
+        view.backgroundColor = .systemBackground
         navigationItem.title = "NYT Bestsellers"
         
         nytBestSellerView.collectionView.dataSource = self
-        //nytBestSellerView.collectionView.delegate = self
+        nytBestSellerView.collectionView.delegate = self
         
         nytBestSellerView.pickerView.dataSource = self
         nytBestSellerView.pickerView.delegate = self
         
-        //FIXME: when I will have cell name
-//        nytBestSellerView.collectionView.register(UINib(nibName: "BookCell", bundle: nil),  forCellWithReuseIdentifier: "bookCell")
+        nytBestSellerView.collectionView.register(BookCell.self, forCellWithReuseIdentifier: "bookCell")
         
-        // register a collectionView cell
-        //         we use generic cell
-    nytBestSellerView.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "bookCell")
-    
+        //FIXME: if we useing user Preference for user defaults
+        //let userCategoryName = userPreference.getCategoryName() ?? "Hardcover Nonfiction"
+        let userCategoryName = "Hardcover Nonfiction"
+        
+        fetchBooks(userCategoryName)
+        //fetchBooks("Hardcover Nonfiction")
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        //fetchBooks()
+    }
+    
+    private func fetchBooks(_ category: String) {
+        NYTAPIClient.getBookData(category) {[weak self] (result) in
+            switch result {
+            case .failure(let appError):
+                print("fetching books error: \(appError)")
+            case .success(let books):
+                DispatchQueue.main.async {
+                    self?.bookData = books
+                }
+            }
+        }
+    }
 }
 
 extension NYTBestsellersController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //FIXME: I should change it with books.count
-        return 10
+        return bookData.count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "bookCell", for: indexPath)
-        cell.backgroundColor = .white
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "bookCell", for: indexPath) as? BookCell else {
+            fatalError("coudl not downcast to BookCell")
+        }
+        let book = bookData[indexPath.row]
+        cell.configureBookCell(book)
+        cell.backgroundColor = .systemBackground
         return cell
     }
 }
@@ -55,39 +98,48 @@ extension NYTBestsellersController: UICollectionViewDataSource {
 extension NYTBestsellersController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let maxSize: CGSize = UIScreen.main.bounds.size
-        let itemHeight: CGFloat = maxSize.height
-        let itemWidth: CGFloat = maxSize.height * 0.5
+        let itemHeight: CGFloat = maxSize.height * 0.20
+        let itemWidth: CGFloat = maxSize.width
         return CGSize(width: itemWidth, height: itemHeight)
     }
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        //FIXME:
-//        // let podcast = podcasts[indexPath.row]
-//        //guard let bookDetailVC =
-//        navigationController?.pushViewController(bookDetailVC, animated: true)
-//    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let book = bookData[indexPath.row]
+        let bookDetailVC = BookDetailViewController()
+        //FIXME: What name of bookData in the BookDetailViewController?
+        //bookDetailVC.book = book
+        //FIXME: uncomment when connest to TabBArController and instance of DataPersistence
+        //bookDetailVC.dataPersistence = dataPersitence
+        navigationController?.pushViewController(bookDetailVC, animated: true)
+    }
 }
 
-    extension NYTBestsellersController: UIPickerViewDataSource {
-        func numberOfComponents(in pickerView: UIPickerView) -> Int {
-            return 1
-        }
-        
-        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-            
-            // FIXME: I will need to add books.count
-            //return 52
-            return categories.count
-        }
+extension NYTBestsellersController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+}
 
 extension NYTBestsellersController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
         // FIXME:
-        return categories[row]
+        return ""//categories[row]
     }
-//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//       // _ = categories[row]
-//    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let categoryName = categories[row]
+        //userPreference.setSectionName(categoryName)
+    }
 }
+
+//FIXME: when we add user defaults:
+//extension NYTBestsellersController: UserPreferenceDelegate {
+//    func didChangeNewsSection(_ userPreference: UserPreference, sectionName: String) {
+//      fetchBooks(userCategoryName)
+//    }
+//}
 
 
